3e6a0616cc97996bd64b06e9316f3f91656e4318
raymondumbas <raymond.umbas@gmail.com>
2023-12-19 09:28:46 -0800

    initial commit

--- main_app.py
+++ main_app.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+
 root = tk.Tk()
 root.title("Budget Tracker")
 currFrame = tk.Frame()
@@ -9,13 +10,6 @@ def show_frame(curr,next):
     currFrame = next
 
 
-
-
-frame1 = tk.Frame(root, width=200, height=100, bg="lightblue")
-frame1.pack();
-frame2 = tk.Frame(root, width=200, height=100, bg="lightgreen")
-button_frame2 = tk.Button(root, text="Show Frame 2", command=lambda: show_frame(frame1, frame2))
-button_frame2.pack(pady=10)
 
 
 
